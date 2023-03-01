f0d1d5612a0b8f42eca21d49345e7c9547c0d7a0
unknown <jcf.daw@gmail.com>
2023-03-01 14:11:11 +0100

    first commit

---
+++ datatypes.js
@@ -0,0 +1,11 @@
+let stri = 'string';
+let num1 = 89;
+let bool1 = true;
+let undf1 = undefined;
+let nulo1 = null;
+
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof bool);
+console.log(typeof undf);
+console.log(typeof nulo);
---
+++ variable.js
@@ -0,0 +1,6 @@
+let str = 'string';
+let num = 89;
+let bool = true;
+let undf = undefined;
+let nulo = null;
+
