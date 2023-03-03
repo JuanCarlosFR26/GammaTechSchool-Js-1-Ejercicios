1670047878020e51ee07da199fa7af50864ed80e
unknown <jcf.daw@gmail.com>
2023-03-03 03:03:30 +0100

    correction

--- code.js
+++ code.js
@@ -1,52 +1,42 @@
-/** Day 1: Exercises
-    - Write a single line comment which says, comments can make code readable
-    - Write another single comment which says, Welcome to 30DaysOfJavaScript
-    - Write a multiline comment which says, comments can make code readable, easy to reuse and informative
-    - Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
-    - Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
-    - Declare four variables without assigning values
-    - Declare four variables with assigned values
-    - Declare variables to store your first name, last name, marital status, country and age in multiple lines
-    - Declare variables to store your first name, last name, marital status, country and age in a single line
-    - Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
- */
+/** Day 1: Exercises */
 
-    // Results:
+// - Write a single line comment which says, comments can make code readable
 // Coments can make code readable
-// Weolcome to 30DaysOfJavaScript
+
+// - Write another single comment which says, Welcome to 30DaysOfJavaScript
+// Welcome to 30DaysOfJavaScript
+
+// - Write a multiline comment which says, comments can make code readable, easy to reuse and informative
 /** Comments can make code readable,
  * easy to refuse and information
  */
+// - Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
 
-// ========================================== //
+// - Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
 
+// - Declare four variables without assigning values
 let game;
 let bank;
 let park;
 let garage;
 
-// ========================================== //
-
+// - Declare four variables with assigned values
 let animanl = 'dog';
 let city = 'Madrid';
 let bird = 'eagle';
 let email = 'jcf.daw@gmail.com';
 
-// ========================================== //
-
+// - Declare variables to store your first name, last name, marital status, country and age in multiple lines
 let firstName = 'Juanqui';
 let lastName = 'Fernández';
 let maritalStatus = 'Single';
 let country = 'Spain';
 let age = 27;
 
-// ========================================== //
-
+// - Declare variables to store your first name, last name, marital status, country and age in a single line
 let name = 'Juanqui', lName = 'Fernández', marital = 'single', ctry = 'Spain', myActualAge = 25;
 
-
-// ========================================== //
-
+// - Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
 let myAge = 27;
 let yourAge = 30;
 
